074cf61213781283c9533d43e775bb9f51e90f46
MehediMK <mehedikhan.cse@gmail.com>
2024-11-19 10:14:39 +0600

    Updated 17 manifest version and support email

--- dynamic_template_renderer/__manifest__.py
+++ dynamic_template_renderer/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Dynamic Template Renderer',
-    'version': '1.5',
+    'version': '17.0.1.0.0',
     'summary': 'Reusable template rendering with dynamic placeholders.',
     'description': """
         Dynamic Template Module
@@ -12,8 +12,9 @@
         - Handles date and datetime formatting.
     """,
     'author': 'Mehedi Khan',
-    'price': '10.0',
+    'price': 10.0,
     'currency': 'USD',
+    'support': 'mehedikhan.cse@gmail.com',
     'website': 'https://apps.odoo.com/apps/browse?repo_maintainer_id=717323',
     'category': 'Tools',
     'license': 'OPL-1',
